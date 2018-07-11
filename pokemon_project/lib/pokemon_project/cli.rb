b6b96e7e2cb57c3e0b341f49f567296b106ec44e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-11 19:03:07 +0000

    added if statement to Pokemon class

--- pokemon_project/lib/pokemon_project/cli.rb
+++ pokemon_project/lib/pokemon_project/cli.rb
@@ -13,6 +13,19 @@ class Pokemon
   end
 
   def more?
-    puts "Please choose which pokemon you would like to know more about"
+    input = nil
+    while input != "exit"
+      puts "Please choose which pokemon you would like to know more about. When done, please enter 'exit'"
+      input = gets.strip
+      if input == "1"
+        puts "1. Pikachu - Electric Type - Can Evolve into Raichu"
+      elsif input == "2"
+        puts "2. Bulbasaur - Grass Type - Can Evolve into Ivysaur"
+      elsif input == "3"
+        puts "3. Geodude - Rock Type - Can Evolve into Graveler"
+      else
+        puts "Invalid input, please try again."
+      end
+    end
   end
 end
