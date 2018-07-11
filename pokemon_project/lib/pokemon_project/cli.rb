dee52398656950e11dd2ff0fff206be8f709f838
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-11 18:53:02 +0000

    rearranged the Pokemon class

--- pokemon_project/lib/pokemon_project/cli.rb
+++ pokemon_project/lib/pokemon_project/cli.rb
@@ -1,7 +1,18 @@
 class Pokemon
+
+  def call
+    puts "Here is a list of some popular Pokemon:"
+    list
+    more?
+  end
+
   def list
    puts "1. Pikachu"
    puts "2. Bulbasaur"
    puts "3. Geodude"
+  end
+
+  def more?
+    puts "Please choose which pokemon you would like to know more about"
   end
 end
